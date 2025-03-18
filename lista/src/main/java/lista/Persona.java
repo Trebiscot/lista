2730a02a6ff4353f1226f20e7185474507984e39
ared <redilevendi.dev@gmail.com>
2025-03-18 18:04:10 +0100

    added db conn

--- lista/src/main/java/lista/Persona.java
+++ lista/src/main/java/lista/Persona.java
@@ -1,5 +1,45 @@
 package lista;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceException;
+
 public class Persona {
-    System.out.println("ciao");
+    private static final Dotenv dotenv = Dotenv.configure().directory("lista").load();
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * private void testConn() {
+     * Map<String, Object> prop = new HashMap<>();
+     * prop.put("jakarta.persistence.jdbc.url",
+     * "jdbc:mysql://localhost:" + dotenv.get("DB_PORT") + "/" +
+     * dotenv.get("DB_NAME"));
+     * prop.put("jakarta.persistence.jdbc.user", dotenv.get("DB_USER"));
+     * prop.put("jakarta.persistence.jdbc.password", dotenv.get("DB_PASSWORD"));
+     * prop.put("jakarta.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
+     * 
+     * try {
+     * 
+     * EntityManagerFactory emf =
+     * Persistence.createEntityManagerFactory("persistence", prop);
+     * EntityManager em = emf.createEntityManager();
+     * 
+     * em.createNativeQuery("SELECT 1").getResultList();
+     * System.out.println("Database connection successful.");
+     * 
+     * em.close();
+     * emf.close();
+     * } catch (PersistenceException e) {
+     * e.printStackTrace();
+     * }
+     * }
+     */
 }
